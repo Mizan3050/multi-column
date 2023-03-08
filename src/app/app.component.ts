9cee76c794c072dc5a035b3f132f1cfb5c9798e0
Meezan Shaikh <meezan@tradexa.co.in>
2023-03-08 20:56:44 +0530

    something done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'multi-column';
+  myData = [
+    {
+      data: {
+        col1: 'row1Col1',
+        col2sub1: 'row1Col2',
+        col2sub2: 'row1Col3',
+        col3sub2: 'row1Col4',
+      },
+    },
+    {
+      data: {
+        col1: 'row2Col1',
+        col2sub1: 'row2Col2',
+        col2sub2: 'row2Col3',
+        col3sub2: 'row2Col4',
+      },
+    },
+  ];
+
+  myColumns = [
+    { key: 'col2', isMulti: true }
+  ];
+
+  mainHeaderColumns = ['prod', 'stock'];
+  subHeaderColumns = ['col2sub1', 'col2sub2', 'col3sub2'];
+
+  allColumns = [
+    {
+      key: "col2",
+      "rowspan": 1,
+      "colspan": 2
+    },
+    {
+      key: "col2sub1",
+      "rowspan": 1,
+      "colspan": 1
+    },
+    {
+      key: "col2sub2",
+      "rowspan": 1,
+      "colspan": 1
+    }
+  ];
+
+  allComplexDataCols = [];
+
+  columnHighlight = true;
+
+  constructor() { }
+
+  ngOnInit() {
+    // $('.mat-column-col2sub1').css('background-color', 'green');
+    this.allComplexDataCols.push(...this.mainHeaderColumns);
+
+    this.myColumns.forEach((col) => {
+      console.log(col);
+      this.mainHeaderColumns.push(col.key);
+    });
+
+    console.log(this.mainHeaderColumns);
+
+    this.allComplexDataCols.push(...this.subHeaderColumns);
+    console.log('allColumns: ', this.allColumns);
+  }
+
 }
