82105f7dd171a5ee11efac0224f6addc78ec67ad
Meezan Shaikh <meezan@tradexa.co.in>
2023-04-03 12:39:01 +0530

    images to pdf added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImageToPdfComponent } from './image-to-pdf/image-to-pdf.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'image-to-pdf',
+    component:ImageToPdfComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
